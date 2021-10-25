aa4bc2a8c4b086b7ff9fae2de8a9c6155173cd85
tmoore28 <mooretylor28@gmail.com>
2021-10-25 17:32:25 -0400

    method exercise

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favBand = Console.ReadLine();
+            Console.WriteLine($"There was a kid named {userName}.");
+            Console.WriteLine($"Every day he would jam along with {favBand} on his guitar.");
+            Console.WriteLine($"The guitar was a birthday gift it was a {favColor} gibson, with a bunch of {favAnimal} stickers all over the back.");
+
+        }
+    }
+}
